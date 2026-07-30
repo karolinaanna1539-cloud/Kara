2c50e81e31cd5c54d813f43b3db33c170cffba2f
karolinaanna1539-cloud <karolinaanna1539@gmail.com>
2026-07-30 21:06:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,86 @@
 function login(){
 
-let password = document.getElementById("password").value;
-
-let message = document.getElementById("message");
-
-
-if(password === "KARA2026"){
-
-    message.innerHTML = `
-    <div class="loading">
-    
-    <p>ACCESS GRANTED</p>
-
-    <p id="loadingText">
-    VERIFYING IDENTITY...
-    </p>
-
-    <div class="bar">
-        <div class="progress"></div>
-    </div>
-
-    </div>
-    `;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
 
 
-    setTimeout(function(){
-        document.getElementById("loadingText").innerHTML =
-        "LOADING PERSONAL DATABASE...";
-    },1000);
+    if(password === "KARA2026"){
 
 
-    setTimeout(function(){
-        document.getElementById("loadingText").innerHTML =
-        "PROFILE....... ONLINE";
-    },2000);
+        message.innerHTML = `
+        <div class="loading">
+            <p>ACCESS GRANTED</p>
+            <p id="loadingText">VERIFYING IDENTITY...</p>
+
+            <div class="bar">
+                <div class="progress"></div>
+            </div>
+        </div>
+        `;
 
 
-    setTimeout(function(){
-        document.getElementById("loadingText").innerHTML =
-        "PHOTOS........ UNLOCKED";
-    },3000);
+        setTimeout(function(){
+
+            document.getElementById("loadingText").innerHTML =
+            "LOADING PERSONAL DATABASE...";
+
+        },1000);
 
 
-    setTimeout(function(){
-        document.getElementById("loadingText").innerHTML =
-        "PROJECTS...... READY";
-    },4000);
+
+        setTimeout(function(){
+
+            document.getElementById("loadingText").innerHTML =
+            "IDENTITY CHECK....... OK";
+
+        },2000);
 
 
-    setTimeout(function(){
 
-        document.getElementById("loadingText").innerHTML =
-        "DATABASE ACCESS COMPLETE";
+        setTimeout(function(){
 
-    },5000);
+            document.getElementById("loadingText").innerHTML =
+            "PROFILE FILE........ ONLINE";
 
-
-    setTimeout(function(){
-
-        window.location.href="archive.html";
-
-    },6500);
+        },3000);
 
 
-}
+
+        setTimeout(function(){
+
+            document.getElementById("loadingText").innerHTML =
+            "VISUAL ARCHIVE...... UNLOCKED";
+
+        },4000);
 
 
-else{
 
-    message.innerHTML =
-    "ACCESS DENIED";
+        setTimeout(function(){
 
-}
+            document.getElementById("loadingText").innerHTML =
+            "DATABASE ACCESS..... COMPLETE";
+
+        },5000);
+
+
+
+        setTimeout(function(){
+
+            window.location.href="archive.html";
+
+        },7000);
+
+
+    }
+
+
+    else {
+
+
+        message.innerHTML =
+        "ACCESS DENIED";
+
+
+    }
 
 }
